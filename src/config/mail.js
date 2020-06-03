9edbb25972ecaa1b34d1af79de01897484140fd7
LucianoFernandesBrido <bridoluciano@gmail.com>
2020-06-03 17:05:38 -0400

    CreateVariablesFile

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '4f3004d2d84ace',
-    pass: '02c5ef67e55779',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,4 +1,3 @@
 export default {
-  dsn:
-    'https://a4e286b3374942fb8c8b9eae45e2df1c@o398888.ingest.sentry.io/5255302',
+  dsn: process.env.SENTRY_DSN,
 };
